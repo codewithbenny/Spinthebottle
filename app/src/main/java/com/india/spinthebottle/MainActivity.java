530ef95dba3ff91e16e16e7554c5fef99f4b36a7
codewithbenny <codewithbenny999@gmail.com>
2020-06-09 15:19:10 +0530

    Spin The Bottle

---
+++ app/src/main/java/com/india/spinthebottle/MainActivity.java
@@ -0,0 +1,60 @@
+package com.india.spinthebottle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private ImageView imageView;
+    private Random random = new Random();
+    private float lastDirection;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+
+    }
+
+    public void spin(View v)
+    {
+        float newDirection = random.nextInt(3600);
+        float pivoitX = imageView.getWidth()/2;
+        float pivoitY = imageView.getHeight()/2;
+        if (Math.abs(lastDirection - newDirection) < 360)
+        {
+            newDirection = lastDirection + newDirection + 360;
+        }
+        Animation rotate = new RotateAnimation(lastDirection,  newDirection, pivoitX, pivoitY);
+
+        rotate.setDuration(2000);
+        rotate.setFillAfter(true);
+        rotate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                button.setEnabled(false);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                button.setEnabled(true);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        lastDirection = newDirection;
+        imageView.startAnimation(rotate);
+    }
+}
